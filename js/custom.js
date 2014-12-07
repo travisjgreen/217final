2235607c7fa79365873928d66d71267102a842fd
Brandon G.A <brandonkeoni@gmail.com>
2014-12-06 20:49:20 -0800

    Removed 'Norway' from start location.

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ $(function(){
             var lat = pos.coords.latitude;
             var lng = pos.coords.longitude;
 //            var map = '<iframe class="img-responsive" src="https://www.google.com/maps?saddr='+lat+'+'+lng+'&daddr=2427+North+Stevens+Street+Spokane+WA+99205&output=embed" height="300" frameborder="0" style="border:0"></iframe>';
-            var url = 'https://www.google.com/maps/embed/v1/directions?key=AIzaSyDCSXNtDdcEe1ZqU2ZcODV-WjBgN0YvsbA&origin='+lat+'+'+lng+'Norway&destination=2427+North+Stevens+Street+Spokane+WA+99205&avoid=tolls|highways';
+            var url = 'https://www.google.com/maps/embed/v1/directions?key=AIzaSyDCSXNtDdcEe1ZqU2ZcODV-WjBgN0YvsbA&origin='+lat+'+'+lng+'&destination=2427+North+Stevens+Street+Spokane+WA+99205&avoid=tolls|highways';
             $('#gmap').attr('src', url);
         });
     }
